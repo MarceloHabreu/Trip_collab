61217fd86c03c461b9d1e030159277849ec05146
MarceloHabreu <marcelohenriqueas17@gmail.com>
2025-03-31 16:28:43 -0300

    🔄 refactor: Remove UpdatePostDto, UpdateUserDto e UserDetailsResponse

--- src/main/java/io/github/marcelohabreu/tripCollab/dtos/UpdatePostDto.java
+++
@@ -1,5 +0,0 @@
-package io.github.marcelohabreu.tripCollab.dtos;
-
-public record UpdatePostDto(String title, String content, String location) {
-
-}
--- src/main/java/io/github/marcelohabreu/tripCollab/dtos/UpdateUserDto.java
+++
@@ -1,4 +0,0 @@
-package io.github.marcelohabreu.tripCollab.dtos;
-
-public record UpdateUserDto(String username, String email, String password) {
-}
